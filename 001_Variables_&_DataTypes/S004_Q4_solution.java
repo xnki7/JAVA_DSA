9ee75a7d60449eb5a82150c77b8a314019516700
Ankit Gupta <92376931+xnki7@users.noreply.github.com>
2022-12-08 18:58:08 +0530

    Add files via upload

---
+++ 001_Variables_&_DataTypes/S004_Q4_solution.java
@@ -0,0 +1,17 @@
+/*
+Type Conversion in java :
+ Java provides various data types just likely any other dynamic languages such as boolean, char, int, unsigned int, signed int, float, double, long, etc in total providing 7 types where every datatype acquires different space while storing in memory. When you assign a value of one data type to another, the two types might not be compatible with each other. If the data types are compatible, then Java will perform the conversion automatically known as Automatic Type Conversion, and if not then they need to be cast or converted explicitly. For example, assigning an int value to a long variable. ~source - GeekforGeeks
+
+boolean	-  1 bit
+byte	-  8 bits (1 byte)
+char	- 16 bits (2 bytes)
+short	- 16 bits (2 bytes)
+int	    - 32 bits (4 bytes)
+long	- 64 bits (8 bytes)
+float	- 32 bits (4 bytes)
+double	- 64 bits (8 bytes)
+
+So, according to the Q4, the result variable will be of "double type" because oftype conversion.
+*/
+
+
