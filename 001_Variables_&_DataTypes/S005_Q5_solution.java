45959b2dbd5e075e31f430a18b99a8f89df9784b
Ankit Gupta <92376931+xnki7@users.noreply.github.com>
2022-12-08 19:03:34 +0530

    Add files via upload

---
+++ 001_Variables_&_DataTypes/S005_Q5_solution.java
@@ -0,0 +1,7 @@
+/*
+No,
+The statement will not give any error.
+Names of variables are called identifiers in Java.
+Identifier rule says, identifiers can start with any alphabet or underscore (“_”) or dollar (“$”).
+According to the rule the given variable name is a valid identifier.
+*/
