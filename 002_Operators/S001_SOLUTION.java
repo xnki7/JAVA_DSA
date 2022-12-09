cd999fa8b24445bdce53ad07253b9e7fe977afe3
Ankit Gupta <92376931+xnki7@users.noreply.github.com>
2022-12-09 21:24:13 +0530

    Add files via upload

---
+++ 002_Operators/S001_SOLUTION.java
@@ -0,0 +1,14 @@
+/*
+
+Solution 1 --> output is : 5, 4
+
+Solution 2 --> output is : Java
+
+Solution 3 --> output is : 4 0 0
+
+Solution 4 --> output is : 278
+
+Solution 5 --> output is : 20, 20
+
+*/
+
