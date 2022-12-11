f87326aa02784dccf55b108345c2236115c50b5e
Ankit Gupta <92376931+xnki7@users.noreply.github.com>
2022-12-11 10:15:28 +0530

    solve b1

---
+++ 004_Loops/B1_BonousQuestion01.java
@@ -0,0 +1,16 @@
+// QUESTION - Write a program to reverse a number entered by user.
+
+import java.util.*;
+class HelloWorld {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number you want to reverse");
+        int n = sc.nextInt();
+        System.out.print("Reversed number is: ");
+        while(n>0){
+            int last_no = n%10;
+            System.out.print(last_no);
+            n = n/10;
+        }
+    }
+}
