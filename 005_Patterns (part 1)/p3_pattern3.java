6c08b794a0a978e01e1eb454c56209a930bf90ec
Ankit Gupta <92376931+xnki7@users.noreply.github.com>
2022-12-12 10:16:01 +0530

    p3

---
+++ 005_Patterns (part 1)/p3_pattern3.java
@@ -0,0 +1,23 @@
+/*
+Print the following pattern:
+
+1
+12
+123
+1234
+
+*/
+
+import java.util.*;
+public class p3_pattern3{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in); 
+        for(int i =1; i<=4; i++){
+            for(int j=1; j<=i; j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        } 
+    } 
+    
+}
