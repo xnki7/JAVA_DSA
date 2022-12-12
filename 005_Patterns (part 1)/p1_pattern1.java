e82da8f2ea7e6e0e1acd1db3ec1ef9d941488269
Ankit Gupta <92376931+xnki7@users.noreply.github.com>
2022-12-12 10:06:52 +0530

    p1

---
+++ 005_Patterns (part 1)/p1_pattern1.java
@@ -0,0 +1,22 @@
+/*
+Print the following pattern:
+*
+**
+***
+****
+
+*/
+
+import java.util.*;
+public class p1_pattern1{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in); 
+        for(int i =1; i<=4; i++){
+            for(int j=1; j<=i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        } 
+    } 
+    
+}
