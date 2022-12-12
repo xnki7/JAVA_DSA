93c24e34dd4f5dc5356c2650fbf4fea727bca31a
Ankit Gupta <92376931+xnki7@users.noreply.github.com>
2022-12-12 10:11:56 +0530

    Add files via upload

---
+++ 005_Patterns (part 1)/p2_pattern2.java
@@ -0,0 +1,22 @@
+/*
+Print the following pattern:
+****
+***
+**
+*
+
+*/
+
+import java.util.*;
+public class p2_pattern2{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in); 
+        for(int i =1; i<=4; i++){
+            for(int j=1; j<=5-i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        } 
+    } 
+    
+}
