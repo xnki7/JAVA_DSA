8ca726969f90b7580c8037c66a21e48367ba0f1a
Ankit Gupta <92376931+xnki7@users.noreply.github.com>
2022-12-13 11:00:13 +0530

    s4

---
+++ 006_Functions_&_Methods/S004_Q4_Solution.java
@@ -0,0 +1,23 @@
+/*
+Use the following methods in your code
+a. Math.min( )
+b. Math.max( )
+c. Math.sqrt( )
+d. Math.pow( )
+e. Math.avg( )
+f. Math.abs( )
+
+All these methods are present in Math class of java
+*/
+
+import java.util.*;
+public class S004_Q4_Solution{
+    public static void main(String args[]){
+        System.out.println(Math.min(4,7));
+        System.out.println(Math.max(4,7));
+        System.out.println(Math.sqrt(9));
+        System.out.println(Math.pow(2,4));
+        // System.out.println(Math.avg(12,15));
+        System.out.println(Math.abs(-4));
+    }
+}
