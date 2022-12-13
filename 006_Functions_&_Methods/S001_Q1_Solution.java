e0ba143842d2bbc4eb6316c0ada93a89b0b62c74
Ankit Gupta <92376931+xnki7@users.noreply.github.com>
2022-12-13 10:35:32 +0530

    s1

---
+++ 006_Functions_&_Methods/S001_Q1_Solution.java
@@ -0,0 +1,10 @@
+// Write a Java method to compute the average of three numbers.
+
+public class S001_Q1_Solution{
+    public static int avg(int a, int b, int c){
+        return (a+b+c)/3;
+    }
+    public static void main(String args[]){
+        System.out.println(avg(2,3,4));
+    }
+}
