0afe470a8cefd74a3b4cc76f80b727040b7cf42f
Ankit Gupta <92376931+xnki7@users.noreply.github.com>
2022-12-14 09:45:24 +0530

    q3 updated

--- 007_Patterns (part 2)/Q3_Inverted_half_pyramid_withNum.java
+++ 007_Patterns (part 2)/Q3_Inverted_half_pyramid_withNum.java
@@ -1,10 +1,10 @@
 /*
 Print the following pattern:
 
-12345
-1234
-123
-12
+1 2 3 4 5
+1 2 3 4
+1 2 3
+1 2
 1
 
 */
@@ -13,10 +13,7 @@ public class Q3_Inverted_half_pyramid_withNum{
     public static void pyramid(int rows){
         for(int i=1; i<=rows; i++){
             for(int j=1; j<=(rows-i+1); j++){
-                System.out.print(j);
-            }
-            for(int j=1; j<i; j++){
-                System.out.print(" ");
+                System.out.print(j+" ");
             }
             System.out.println();
         }
