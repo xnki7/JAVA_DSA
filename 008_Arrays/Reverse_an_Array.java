e6c86139369e2d196146b5ff3e81e1cdb282abf6
Ankit Gupta <92376931+xnki7@users.noreply.github.com>
2022-12-17 09:31:38 +0530

    Add files via upload

---
+++ 008_Arrays/Reverse_an_Array.java
@@ -0,0 +1,24 @@
+//Write a program to reverse an array.
+
+public class Reverse_an_Array{
+    public static void reverse(int arr[]){
+        int first = 0;
+        int last  = arr.length -1;
+
+        while(first<last){
+            int temp = arr[first];
+            arr[first] = arr[last];
+            arr[last] = temp;
+            first++;
+            last--;
+        }
+    }
+    
+    public static void main(String args[]){
+        int arr[] = {1,2,3,4,5};
+        reverse(arr);
+        for(int i=0; i<arr.length; i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
