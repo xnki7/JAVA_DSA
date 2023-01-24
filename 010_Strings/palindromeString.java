69c580d5622816a0916bb1407ed6510568762fb5
Ankit Gupta <92376931+xnki7@users.noreply.github.com>
2023-01-24 10:11:14 +0530

    Create palindromeString.java

---
+++ 010_Strings/palindromeString.java
@@ -0,0 +1,16 @@
+public class palindromeString{
+    public static boolean isPalindrome(String str){
+        int n = str.length();
+        for(int i=0; i<n/2; i++){
+            if(str.charAt(i) != str.charAt(n-1-i)){
+                return false;
+            }
+
+        }
+        return true;
+    }
+    public static void main(String args[]){
+        String str = "racecar";
+        System.out.println(isPalindrome(str));
+    }
+}
